32ca20426a2ba2ad2e6d6433c5067c36554c168d
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-30 18:05:05 +0500

    Настройка конфига

--- sso/internal/config/config.go
+++ sso/internal/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Config struct {
 	Env      string        `yaml:"env" env-default:"local"`
@@ -11,4 +17,36 @@ type Config struct {
 type GRPCConfig struct {
 	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path in empty")
+	}
+
+	if _, err := os.Stat(path); err != nil {
+		panic("config file does not exit: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
 }
--- sso/storage/database.go
+++ sso/storage/database.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,11 +9,13 @@ import (
 var DB *gorm.DB
 
 func InitGormDB() *gorm.DB {
-	dsn := "user=postgres password=123456 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Taipei"
+	dsn := "user=postgres password=8008 dbname=grpc_remzona port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return err
+		logrus.Fatal("Error connect to database: ", err)
+		return nil
 	}
+	logrus.Info("Server connet to database: " + "grpc_remzona")
 
 	DB = db
 
