dbfbf61182ec27909fe09bfbcf2ed47ca33f3db6
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-30 23:54:37 +0500

    Implemented register method in the service auth

--- sso/internal/services/auth/auth.go
+++ sso/internal/services/auth/auth.go
@@ -2,10 +2,12 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"sso/internal/domain/models"
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Auth struct {
@@ -51,17 +53,36 @@ func (a *Auth) Login(
 	email, password string,
 	appID int64,
 ) (string, error) {
-
-	return "", nil
+	panic("not implementet")
 }
 
 func (a *Auth) RegisterNewUser(
 	ctx context.Context,
 	email, pass string,
 ) (int64, error) {
-	panic("no implementet")
+	const op = "auth.RegisterNewUser"
+
+	log := a.log.WithFields(logrus.Fields{
+		"op": op,
+	})
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hashe: ", err)
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user: ", err)
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("user registered")
+
+	return id, nil
 }
 
 func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
-	panic("no implementet")
+	panic("not implementet")
 }
