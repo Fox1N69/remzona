02bca8e83bbb8e63de570fe6bad1d8d3dfe8f8ab
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-06-30 22:34:49 +0500

    Create login handler and validation function and auth interface

--- sso/internal/grpc/auth/server.go
+++ sso/internal/grpc/auth/server.go
@@ -5,24 +5,55 @@ import (
 
 	ssov1 "github.com/Fox1N69/remzona-protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(ctx context.Context, email, password string, appID int) (token string, err error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
-	panic("implement me")
+
+	if err := validationLogin(req); err != nil {
+		return nil, status.Error(codes.Internal, "error validation")
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.LoginResponse{
+		Token: token,
+	}, nil
+}
+
+func validationLogin(req *ssov1.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email in empty")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is empty")
+	}
+
+	if req.AppId == 0 {
+		return status.Error(codes.InvalidArgument, "appId is nil")
+	}
+
+	return nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
-	panic("implement me")
-}
-
-func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
 	panic("implement me")
 }
