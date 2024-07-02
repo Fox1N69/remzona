64c5632649867830d3c60c3edb977208c58cd808
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-07-02 19:37:48 +0500

    new service

---
+++ services/sso/internal/manager/repo_manager.go
@@ -0,0 +1,30 @@
+package manager
+
+import (
+	"sync"
+
+	"shop-server/infra"
+)
+
+type RepoManager interface {
+}
+
+type repoManager struct {
+	infra infra.Infra
+}
+
+func NewRepoManager(infra infra.Infra) RepoManager {
+	return &repoManager{infra: infra}
+}
+
+var (
+	orderRepoOnce sync.Once
+	orderRepo     repo.OrderRepo
+)
+
+func (rm *repoManager) OrderRepo() repo.OrderRepo {
+	orderRepoOnce.Do(func() {
+		orderRepo = repo.NewOrderRepo(rm.infra.GormDB())
+	})
+	return orderRepo
+}
